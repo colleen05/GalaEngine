83344ece2bcf988c0c0fadc6abfd5490a082d02d
Colleen <cdnickerson05@gmail.com>
2022-08-29 22:36:21 -0300

    Included raygui for the demo project

--- Demo/src/Main.cpp
+++ Demo/src/Main.cpp
@@ -7,8 +7,31 @@
 #include <Demo_Drawing.hpp>
 
 int main() {
-    GalaEngine::Game *game = new Demo_Drawing();
-    game->Start();
+    GalaEngine::Game *game = nullptr;
+
+    InitWindow(640, 480, "GalaEngine Demos");
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()) {
+        // Logic
+        if(IsKeyPressed(KEY_ONE)) {
+            game = new Demo_Drawing();
+        }
+
+        // Drawing
+        BeginDrawing();
+        ClearBackground(C_BLACK);
+
+        DrawText("[1] - Surface functions.", 8, 16, 20, C_WHITE);
+
+        EndDrawing();
+
+        // If demo is selected...
+        if(game != nullptr) {
+            CloseWindow();
+            game->Start();
+        }
+    }
 
     return 0;
 }
