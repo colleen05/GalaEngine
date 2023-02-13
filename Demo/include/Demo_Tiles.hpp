802c0534f4096b715a1c8911a5fd1ade9143bb34
Colleen <cdnickerson05@gmail.com>
2023-02-13 15:46:19 -0400

    Updated: Tiles demo

--- Demo/include/Demo_Tiles.hpp
+++ Demo/include/Demo_Tiles.hpp
@@ -15,11 +15,45 @@
 
 class Demo_Tiles : public GalaEngine::Game {
     public:
+        const int tileSize = 64;
+        const int tilemapWidth = 9;
+        const int tilemapHeight = 9;
+        const int sidebarWidth = 448;
+
+        enum class EditMode {
+            PlaceTiles,
+            PickTiles
+        };
+        
+        EditMode editMode = EditMode::PlaceTiles;
+
+        enum class EditLayer {
+            Background,
+            Foreground,
+            Both
+        };
+
+        EditLayer targetLayer   = EditLayer::Background;
+        EditLayer viewLayer     = EditLayer::Both;
+
+        GalaEngine::TileLayer *lay_bgTiles = nullptr;
+        GalaEngine::TileLayer *lay_fgTiles = nullptr;
+
+        int selectionX = 0;
+        int selectionY = 0;
+        uint16_t brushTile = 0;
+
+        bool viewFlags = false;
+
         // Game class overrides
         void OnLoad();
         void OnDraw();
         void OnUpdate();
         void OnUnload();
+
+        // Editor
+        void DoPicker();
+        void DrawTileGrid();
 
         // Constructor
         Demo_Tiles();
--- Demo/src/Demo_Tiles.cpp
+++ Demo/src/Demo_Tiles.cpp
@@ -1,27 +1,138 @@
 #include <Demo_Tiles.hpp>
 
+// Game class overrides
 void Demo_Tiles::OnLoad() {
+    // Add background layer
+    auto bgLayer = scene->AddBackgroundLayer(assets->GetTexture("bg_water"));
+    bgLayer->scrollSpeed = {16.0f, 16.0f};
+
+    assets->LoadTileset("environment");
+
+    // Add background tile layer
+    lay_bgTiles = scene->AddTileLayer(
+        assets->GetTileset("environment"),
+        std::vector<uint16_t>(16 * tilemapHeight, 0x00),
+        16, tilemapHeight
+    );
+
+    // Add foreground tile layer
+    lay_fgTiles = scene->AddTileLayer(
+        assets->GetTileset("environment"),
+        std::vector<uint16_t>(16 * tilemapHeight, 0x00),
+        16, tilemapHeight
+    );
+
+    for(int i = 0; i < lay_bgTiles->tiles.size(); i++) {
+        lay_bgTiles->tiles[i] = GetRandomValue(0, 63);
+    }
+
+    lay_bgTiles->Render();
 }
 
 void Demo_Tiles::OnDraw() {
-    float thickness = IsKeyDown(KEY_SPACE) ? 10.0f : 2.0f;
-    window->surface.DrawRectangle(320, 240, 320, 240, C_GALARED, true, thickness);
+    // Draw sidebar.
+    // window->surface.DrawRectangle(0, 0, sidebarWidth, 576, {0x33, 0x33, 0x33, 0xff});
+
+    // Handle edit mode.
+    switch(editMode) {
+        case EditMode::PlaceTiles:
+            break;
+        case EditMode::PickTiles:
+            break;
+    }
+
+    // Draw grid
+    DrawTileGrid();
 }
 
 void Demo_Tiles::OnUpdate() {
+    // Toggle viewing tile flags.
+    if(IsKeyPressed(KEY_GRAVE)) {
+        viewFlags = !viewFlags;
+    }
+
+    // Update brush
+    brushTile += GetMouseWheelMove();
+    if(brushTile > 64) brushTile = 0; 
+
+    // Update selection
+    const Vector2 mousePosition = GetMousePosition();
+    selectionX = (mousePosition.x - sidebarWidth) / tileSize;
+    selectionY = mousePosition.y / tileSize;
+
+    // Handle edit mode.
+    if(editMode == EditMode::PlaceTiles) {
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            sound->Play(assets->GetSound("sfx_chime"));
+
+            GalaEngine::TileLayer *layer =
+                (targetLayer == EditLayer::Background) ?
+                lay_bgTiles : lay_fgTiles;
+
+            const size_t brushIndex = selectionY * tilemapWidth + selectionX;
+            
+            layer->tiles[brushIndex] = brushTile;
+        }
+    }
 }
 
 void Demo_Tiles::OnUnload() {
 
 }
 
+// Editor
+void Demo_Tiles::DoPicker() {
+
+}
+
+void Demo_Tiles::DrawTileGrid() {
+    // Draw vertical lines.
+    for(int i = 0; i < tilemapWidth; i++) {
+        window->surface.DrawLine(
+            sidebarWidth + (tileSize * i), 0,
+            sidebarWidth + (tileSize * i), 576,
+            {0x00, 0x00, 0x00, 0x33}
+        );
+    }
+
+    // Draw horizontal lines.
+    for(int i = 0; i < tilemapHeight; i++) {
+        window->surface.DrawLine(
+            sidebarWidth, tileSize * i,
+            1024, tileSize * i,
+            {0x00, 0x00, 0x00, 0x33}
+        );
+    }
+
+    // Draw brush tile
+    if( (editMode == EditMode::PickTiles) ||
+        (selectionX < 0)
+    ) return;
+
+    window->surface.DrawTexture(
+        assets->GetTileset("environment").texture,
+        (Rectangle) {
+            (brushTile % 8) * 64.0f,
+            (brushTile / 8) * 64.0f,
+            64.0f, 64.0f
+        },
+        (Rectangle) {
+            (float)(sidebarWidth + (tileSize * selectionX)),
+            (float)(tileSize * selectionY),
+            (float)tileSize, (float)tileSize,
+        },
+        {0xff, 0xff, 0xff, 0x55}
+    );
+}
+
+// Constructor
 Demo_Tiles::Demo_Tiles() : Game(
     GalaEngine::GameInfo {
         "GalaEngine Tiles Demo (GalaEngine v" GALAENGINE_VERSION_STRING ")",
         "GalaEngine example project.",
         "Colleen (colleen05)",
 
-        1280, 720,
+        1024, 576,
 
         GalaEngine::AssetPathLayout {
             "./resources/",
