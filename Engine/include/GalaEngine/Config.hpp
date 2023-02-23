bedbb86cd64430c3f87d8c2d3cc391813453d228
Colleen <cdnickerson05@gmail.com>
2023-02-22 23:33:37 -0400

    Added: Config header

---
+++ Engine/include/GalaEngine/Config.hpp
@@ -0,0 +1,10 @@
+// Project: GalaEngine
+// File: Config.hpp
+// Description: Config for GalaEngine
+// Authors: Colleen (@colleen05)
+// https://github.com/colleen05/GalaEngine
+// Distributed under the zlib license.
+
+#pragma end
+
+#define GALAENGINE_DEBUG true
