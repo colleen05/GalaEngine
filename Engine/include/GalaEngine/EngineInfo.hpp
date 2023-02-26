e3ebba0f67dd65588de5c0de1a28329d692a125d
Colleen <cdnickerson05@gmail.com>
2023-02-26 03:17:48 -0400

    Updated: EngineInfo

--- Engine/include/GalaEngine/EngineInfo.hpp
+++ Engine/include/GalaEngine/EngineInfo.hpp
@@ -12,9 +12,9 @@
 #define GALANEGINE_VERSION_PATCH 0
 
 // r = release, a = alpha, b = beta, d = in-development, rc.x = release candidate.
-#define GALAENGINE_VERSION_INDICATOR 'r'
+#define GALAENGINE_VERSION_INDICATOR 'd'
 
-#define GALAENGINE_VERSION_STRING "0.2.0"
+#define GALAENGINE_VERSION_STRING "0.3.0-d"
 
 namespace GalaEngine {
     const char* GetEngineInfo(); //!< Return string containing information about the current version of GalaEngine.
