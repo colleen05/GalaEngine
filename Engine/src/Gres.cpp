18de6b3e2946d76d816724fce34034f002dc4393
Colleen <cdnickerson05@gmail.com>
2023-03-17 17:44:23 -0300

    Implemented: 9-Slice loading.

--- Engine/src/Gres.cpp
+++ Engine/src/Gres.cpp
@@ -128,6 +128,87 @@ GalaEngine::Sprite GalaEngine::Gres::LoadSprite(const std::string &filePath) {
 }
 
 
+// 9-Slices
+GalaEngine::NSlice GalaEngine::Gres::LoadNSliceData(const std::vector<uint8_t> &data) {
+    // Load table
+    xdt::Table gresTable;
+    gresTable.Deserialise(data);
+
+    // Checking
+    if(gresTable.GetString("type")          != "nslice")             return {{0}, {0}, {0}};
+    if(gresTable.GetItemType("texture")     != xdt::ItemType::Raw)   return {{0}, {0}, {0}};
+    
+    if(gresTable.GetItemType("centre_slice.x") != xdt::ItemType::Int16) return {{0}, {0}, {0}};
+    if(gresTable.GetItemType("centre_slice.y") != xdt::ItemType::Int16) return {{0}, {0}, {0}};
+    if(gresTable.GetItemType("centre_slice.w") != xdt::ItemType::Int16) return {{0}, {0}, {0}};
+    if(gresTable.GetItemType("centre_slice.h") != xdt::ItemType::Int16) return {{0}, {0}, {0}};
+
+    if(!gresTable.ItemExists("stretch_slices.up"))      return {{0}, {0}, {0}};
+    if(!gresTable.ItemExists("stretch_slices.right"))   return {{0}, {0}, {0}};
+    if(!gresTable.ItemExists("stretch_slices.down"))    return {{0}, {0}, {0}};
+    if(!gresTable.ItemExists("stretch_slices.left"))    return {{0}, {0}, {0}};
+    if(!gresTable.ItemExists("stretch_slices.centre"))  return {{0}, {0}, {0}};
+
+
+    /// Decoding ///
+    GalaEngine::NSlice nslice;
+
+    // Decode texture
+    const auto textureBytes = gresTable.GetBytes("texture");
+
+    Image img = LoadImageFromMemory(".qoi", textureBytes.data(), textureBytes.size() * sizeof(textureBytes[0]));
+    if( (img.data == NULL) ||
+        (img.width == 0) ||
+        (img.height == 0)
+    ) return {{0}, {0}, {0}};
+
+    nslice.texture = LoadTextureFromImage(img);
+
+    // Set texture filter
+    if(gresTable.ItemExists("texture_filter")) {
+        std::string filterStr = gresTable.GetString("texture_filter");
+        TextureFilter filter = TEXTURE_FILTER_POINT;
+
+        if      (filterStr == "bilinear")   filter = TEXTURE_FILTER_BILINEAR;
+        else if (filterStr == "trilinear")  filter = TEXTURE_FILTER_TRILINEAR;
+
+        SetTextureFilter(nslice.texture, filter);
+    }
+
+    // Unload image
+    UnloadImage(img);
+
+    // Decode centre slice
+    nslice.centreSlice = (Rectangle) {
+        gresTable.GetFloat("centre_slice.x"),
+        gresTable.GetFloat("centre_slice.y"),
+        gresTable.GetFloat("centre_slice.w"),
+        gresTable.GetFloat("centre_slice.h")
+    };
+
+    // Decode stretched slice
+    nslice.stretchSlices[0] = gresTable.GetBool("stretch_slices.up");
+    nslice.stretchSlices[1] = gresTable.GetBool("stretch_slices.right");
+    nslice.stretchSlices[2] = gresTable.GetBool("stretch_slices.down");
+    nslice.stretchSlices[3] = gresTable.GetBool("stretch_slices.left");
+    nslice.stretchSlices[4] = gresTable.GetBool("stretch_slices.centre");
+
+
+    return nslice;
+}
+
+GalaEngine::NSlice GalaEngine::Gres::LoadNSlice(const std::string &filePath) {
+    std::ifstream f(filePath);
+
+    if(!f.good()) return {{0}, {0}, {0}};
+
+    return LoadNSliceData(std::vector<uint8_t>(
+        std::istreambuf_iterator<char>(f),
+        std::istreambuf_iterator<char>()
+    ));
+}
+
+
 // Tilesets
 GalaEngine::Tileset GalaEngine::Gres::LoadTilesetData(const std::vector<uint8_t> &data) {
     // Load table
